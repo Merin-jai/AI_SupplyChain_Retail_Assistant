5a41beb947685ebfd6dd6cff4aa0098cb3d5d978
Merin-jai <mjjm.t01@gmail.com>
2025-08-05 10:53:57 +0530

    Working backend main.py

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+# main.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "AI Supply Chain Assistant Flask Backend is Running!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
